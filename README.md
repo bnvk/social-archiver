8f70e537e711a60221bfe3e8782e5a47903a7fcc
Brennan Novak <hi@brennannovak.com>
2014-06-11 15:31:19 +0200

    Made datetime printed look nice, made it use dynamic profile data for to: user, updated Docs & Readme

--- README.md
+++ README.md
@@ -8,17 +8,37 @@ A simple Python tool that downloads your pictures and private messages from Face
 Unless you already have a Facebook App auth token, this tool will not function correctly and do what it is supposed to. I'm working on this as much as possible, but development takes time. If you are handy with Python, feel free to get in touch and help ~ [Brennan Novak](https://brennannovak.com ) or [@brennannovak](https://github.com/brennannovak )
  
 
-### Setup
+### Initial Setup
 
 Copy `config.yaml.template` and change the name to `config.yaml`
 
-Go "Create a New App" on Facebook's [developer platform](https://developers.facebook.com)
-
-Edit the Facebook config values in config.yaml with the values Facebook gives you
-
 Install python dependencies `$ pip install -r requirements.txt`
 
+Now you have two options with how to proceed. One is using Facebooks Graph Explorer to generate your auth token. The other is to create your own Facebook "App" and generate your auth tokens that way. The first option is fine if you're doing a one time export and deleting your account. The later is better if you want to keep using Facebook and periodically re-download updates.
 
-### Use
+
+### Setup Using Graph Explorer
+
+* Go to Facebook's [Developer Platform](https://developers.facebook.com) page. 
+
+* Edit the Facebook config values in config.yaml with the values Facebook gives you
+
+
+### Setup Using App
+
+* Go to Facebook's [Developer Platform](https://developers.facebook.com)
+
+* Click on top menu "Apps" and then "Create a New App" 
+
+* Open the `config.yaml` file you created in step 1
+
+* Copy the "App ID" on Facebook into the field `facebook_app_id` in config.yaml
+
+* Copy the "App Secret" on Facebook into the field `facebook_secret` in config.yaml
+
+
+### Use The Tool
 
 From the command line run `$ python grab.py`
+
+
--- grab.py
+++ grab.py
@@ -15,6 +15,7 @@ from operator import itemgetter
 from urlparse import urlparse
 from urlparse import parse_qs
 from datetime import datetime
+from time import strptime
 
 # Load App Config Values
 config_yaml = open('config.yaml')
@@ -33,14 +34,15 @@ def make_directories():
         os.makedirs("downloads/facebook/friends/")
     if not os.path.exists("downloads/facebook/friends_photos/"):
         os.makedirs("downloads/facebook/friends_photos/")
-    if not os.path.exists("downloads/facebook/messages/"):
-        os.makedirs("downloads/facebook/messages/")   
-    if not os.path.exists("downloads/facebook/messages_attachments/"):
-        os.makedirs("downloads/facebook/messages_attachments/")   
+    if not os.path.exists("downloads/messages/"):
+        os.makedirs("downloads/messages/")   
+    if not os.path.exists("downloads/messages_attachments/"):
+        os.makedirs("downloads/messages_attachments/")   
     if not os.path.exists("downloads/facebook/you/"):
         os.makedirs("downloads/facebook/you/")
     if not os.path.exists("downloads/facebook/you_photos/"):
         os.makedirs("downloads/facebook/you_photos/")
+
 
 # Saves Profiles To Disk in json & jpg
 def get_profile(pid):
@@ -75,7 +77,7 @@ def get_profile(pid):
         fh2.close()
 
         # Save Profile Data
-        with open(profile_path + profile['id'] + ".json", "w") as outfile:
+        with open(profile_path + pid + ".json", "w") as outfile:
             json.dump(profile, outfile, indent=4)
 
 
@@ -161,17 +163,28 @@ class Photos():
         else:
             print "Empty photos result"        
 
+
 # Conversations
 class Conversations():
 
-    def __init__ (self):
+    def __init__ (self, profile):
         self.current = 0
+
+    def nice_datetime(self, dt):
+        split = dt.split('+')[0]
+        fixed = datetime(*strptime(split, "%Y-%m-%dT%H:%M:%S")[0:6])
+        month = fixed.strftime('%b ')
+        day_year = fixed.strftime('%d, %Y at ').lstrip('0')
+        time = fixed.strftime('%H:%M').lstrip('0')
+        return month + day_year + time
 
     # Process text/plain
     def process_plain(self, message):
         out = ''
         if message['message']:
-            out += message['created_time'] + ", " + message['from']['name'] + " wrote:\n"
+
+            # Format datetime like May 25, 2014 at 3:01 PM
+            out += self.nice_datetime(message['created_time']) + ", " + message['from']['name'] + ":\n"
             out += message['message'] + '\n'
             out += "\n"
         return out
@@ -179,7 +192,7 @@ class Conversations():
     # Process text/html
     def process_html(self, message):
         out = '      <div class="h-entry">\n'
-        out += '        <time class="dt-published" datetime="' + message['created_time'] + '">' + message['created_time'] + '</time>\n'
+        out += '        <time class="dt-published" datetime="' + message['created_time'] + '">' + self.nice_datetime(message['created_time']) + '</time>\n'
         out += '        <a href="mailto:' + message['from']['email'] + '" class="p-author h-card">\n'
         out += '          <span class="p-name">' + message['from']['name'] + '</span>\n'
         out += '          <span class="u-uid" hidden="true">' + message['from']['id'] + '</span>\n'
@@ -215,7 +228,7 @@ class Conversations():
                     response = requests.get(url = attachment['image_data']['url'], stream=True)
                     if response.status_code == 200:
                         attachment_status = 'success'
-                        with open('downloads/facebook/messages_attachments/' + attachment['name'], 'wb') as out_file:
+                        with open('downloads/messages_attachments/' + attachment['name'], 'wb') as out_file:
                             shutil.copyfileobj(response.raw, out_file)
                     del response
                 else:
@@ -232,7 +245,7 @@ class Conversations():
                         attachment_status = 'success'
                         json = response.json()
                         output_file = base64.b64decode(json['data'])
-                        fh2 = open('downloads/facebook/messages_attachments/' + attachment['name'], 'wb')
+                        fh2 = open('downloads/messages_attachments/' + attachment['name'], 'wb')
                         fh2.write(output_file)
                         fh2.close()
                     del response
@@ -252,10 +265,6 @@ class Conversations():
             print 'Now running ' + until
             result = graph.get_object('/me/conversations', limit=100, until=until)
 
-        # Profile
-        # FIXME: make dynamic
-        profile = dict({ 'name': 'Brennan Novak', 'id': '653983917', 'email': '653983917@facebook.com' })
-
         # (Pass in result['conversations']['data'])
         for conversation in result['data']:
 
@@ -265,7 +274,7 @@ class Conversations():
 
             # Container Message
             headers     = 'From social-archiver'
-            header_user = profile['name'] + ' <' + profile['email'] + '>'
+            header_user = profile['name'] + ' <' + profile['id'] + '@facebook.com>'
             header_cc   = []
             names       = []
             plain       = ''
@@ -281,7 +290,7 @@ class Conversations():
                 # Headers
                 for to in message['to']['data']:
                     email = (to['name'] + ' <' + to['email'] + '>')
-                    if email not in header_cc and to['email'] != profile['email']:
+                    if email not in header_cc and to['email'] != profile['id'] + '@facebook.com':
                         header_cc.append(email)
                         names.append(to['name'])
 
@@ -322,13 +331,13 @@ class Conversations():
                         part = writer.nextpart()
                         part.addheader('Content-Transfer-Encoding', 'base64')
                         body = part.startbody(attach['mime'])
-                        base64.encode(open('downloads/facebook/messages_attachments/' + attach['name'], 'rb'), body)
+                        base64.encode(open('downloads/messages_attachments/' + attach['name'], 'rb'), body)
 
             # Finish Email
             writer.lastpart()
 
             # Save TXT
-            mbox = open("downloads/facebook/messages/" + conversation_id, "w")
+            mbox = open("downloads/messages/" + conversation_id, "w")
             mbox.write(message.getvalue().encode('utf-8'))
             mbox.close()
 
@@ -340,19 +349,33 @@ class Conversations():
         self.get(query_string['until'][0])
 
 
+# Do Defaults
+print "Making directories"
 make_directories()
 
-myPhotos = Photos()
-#myPhotos.get_tagged('start')
-myPhotos.test()
+# Get Your Profile
+if os.path.isfile("downloads/facebook/you/me.json"):
+    print "Getting your profile"
+    #get_profile('/me')
+    profile = json.loads(open('downloads/facebook/you/me.json').read())
 
-#myConversations = Conversations() # Instantiate Conversahttp://s3-ec.buzzfed.com/static/2014-06/1/18/enhanced/webdr07/enhanced-5653-1401661233-3.jpgtion Class
-#myConversations.get('start') # Start Conversation Downloading
+# CL args
+if 'friends' in (sys.argv):
+    print "Getting friends data & pictures"
+    get_friends()
 
+if 'photos' in (sys.argv):
+    print "Getting photos you are tagged in"
+    myPhotos = Photos()
+    myPhotos.get_tagged('start')
 
-#def main():
-#if __name__ == "__main__":
-#    main()
+if 'messages' in (sys.argv):
+    print "Getting your messages & attachments"
+    myConversations = Conversations(profile) 
+    myConversations.get('start')
+
+print "Downloading is all done, takk :)"
+
 
 # Geneate a page with this to make manually deleting friends easier
 # <a href="http://m.facebook.com/3621161" onClick="window.open(this.href, this.target, 'width=500,height=600'); return false;"> Unfriend Name</a>
